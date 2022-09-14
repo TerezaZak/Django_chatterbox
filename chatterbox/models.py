c0c9aba0ed2f5c1694378acb46cab69bfc64d498
Tereza Zakourilova <tereza.zakourilova@gmail.com>
2022-09-14 13:03:29 +0200

    Přidány templates

--- chatterbox/models.py
+++ chatterbox/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
+    #host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,7 +27,7 @@ class Message(models.Model):
         ordering = ['-created', '-updated']
 
 
-        def __str__(self):
-            return self.body[0:50]
+    def __str__(self):
+        return self.body[0:50]
 
 
--- chatterbox/views.py
+++ chatterbox/views.py
@@ -9,9 +9,24 @@ def hello(request, s):
     return HttpResponse(f'Hello, {s} world!')
 
 
+def home(request):
+    rooms = Room.objects.all()  # najdeme všechny místnosti
+
+    context = {'rooms': rooms}
+    return render(request, 'chatterbox/home.html', context)
+
+
 def search(request, s):
     rooms = Room.objects.filter(name__contains=s)
     messages = Message.objects.filter(body__contains=s)
 
     context = {'rooms': rooms, 'messages': messages}
     return render(request, "chatterbox/search.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk) # najdeme místnost se zadaným id
+    messages = Message.objects.filter(room=pk) # vybereme všechny zprávy dané místnosti
+
+    context = {'room': room, 'messages': messages}
+    return render(request, "chatterbox/room.html", context)
